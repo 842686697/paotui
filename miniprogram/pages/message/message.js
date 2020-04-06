a923192cb813d15de29e25bf615661bb5c2ef603
842686697 <px842686697@outlook.com>
2020-04-06 21:26:04 +0800

    更新 message user

--- miniprogram/pages/message/message.js
+++ miniprogram/pages/message/message.js
@@ -1,28 +1,18 @@
-
+// pages/message/message.js
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    list:[],
-    collection:'list',
-    groupId:'',
+
   },
+
   /**
    * 生命周期函数--监听页面加载
    */
-  getData:function(){
-    const { collection } = this.data;
-    const db = wx.cloud.database();
-    const _ = db.command;
-    const listData = db.collection(collection).where({}).get({
-      success: res => {
-        console.log(res);
-        this.setData({
-          list: res.data
-        })
-      }
-    })
-  },
   onLoad: function (options) {
-    this.getData();
+
   },
 
   /**
@@ -57,10 +47,7 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    this.getData();
-    setTimeout(function () {
-      wx.stopPullDownRefresh();
-    }, 1000);
+
   },
 
   /**
