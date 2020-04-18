548157a5017bd2f7448a4444f45692bb5eb4d6a6
842686697 <px842686697@outlook.com>
2020-04-18 22:21:53 +0800

    mu_publish

--- miniprogram/pages/my_publish/my_publish.js
+++ miniprogram/pages/my_publish/my_publish.js
@@ -5,14 +5,43 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    openid:'',
+    collection:'list',
+    list:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  getOpenid: function (callback) {
+    //用云函数获取openid
+    wx.cloud.callFunction({
+      name: 'login',
+      success: res => {
+        this.setData({
+          openid: res.result.openid
+        })
+        callback();
+      }
+    })
+  },
+  getData:function(){
+    const { collection, openid } = this.data;
+    const db = wx.cloud.database();
 
+    db.collection(collection).where({
+      _openid: openid
+    }).get({
+      success: res => {
+        console.log(res)
+        this.setData({
+          list: res.data
+        })
+      }
+    })
+  },
+  onLoad: function (options) {
+    this.getOpenid(this.getData);
   },
 
   /**
